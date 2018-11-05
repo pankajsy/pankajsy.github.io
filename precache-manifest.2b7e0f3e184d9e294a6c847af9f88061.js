f628caba5c5acd8469a354991e633ea972dc07d4
pankajsy <psy231@nyu.edu>
2018-11-04 20:49:04 -0500

    react build files for portfolio website

---
+++ precache-manifest.2b7e0f3e184d9e294a6c847af9f88061.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7245a328aef427485ace",
+    "url": "/static/js/main.7245a328.chunk.js"
+  },
+  {
+    "revision": "a7194b2d988a5c80f583",
+    "url": "/static/js/1.a7194b2d.chunk.js"
+  },
+  {
+    "revision": "7245a328aef427485ace",
+    "url": "/static/css/main.c4b4ee74.chunk.css"
+  },
+  {
+    "revision": "b2e552a7ea87ff6dffa6952063912516",
+    "url": "/index.html"
+  }
+];
